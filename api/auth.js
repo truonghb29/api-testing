b6b062d58a8f82f6ce710dfa870dd3fa1a1e08d9
truong.nguyen <truonghb29@gmail.com>
2024-04-18 23:33:20 +0700

    init source

--- api/auth.js
+++ api/auth.js
@@ -2,16 +2,27 @@ import axios from "axios";
 import { expect } from "chai";
 
 describe("API testing Auth", () => {
+  const formData = new FormData();
+  formData.append(
+    "customerSignupInfo",
+    JSON.stringify({
+      email: "thanhtruong",
+      password: "thanhtruong",
+    })
+  );
+
+  const config = {
+    method: "post",
+    url: "https://dynasty-ws.vtaan.id.vn/api/auth/user/register",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "multipart/form-data",
+    },
+    data: formData,
+  };
   it("register", async () => {
     try {
-      const response = await axios({
-        method: "post",
-        url: "https://dynasty-ws.vtaan.id.vn/api/auth/user/register",
-        data: {
-          email: "thanhtruong",
-          password: "thanhtruong",
-        },
-      });
+      const response = await axios(config);
 
       expect(response.status).to.equal(200);
       expect(response.data).to.have.property("token");
@@ -21,15 +32,27 @@ describe("API testing Auth", () => {
   });
 
   it("login", async () => {
+    const formData = new FormData();
+    formData.append(
+      "userLoginInfo",
+      JSON.stringify({
+        username: "admin",
+        password: "13112002",
+      })
+    );
+
+    const config = {
+      method: "post",
+      url: "https://dynasty-ws.vtaan.id.vn/api/auth/user/login",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "multipart/form-data",
+      },
+      data: formData,
+    };
+
     try {
-      const response = await axios({
-        method: "post",
-        url: "https://dynasty-ws.vtaan.id.vn/api/auth/user/login",
-        data: {
-          username: "admin",
-          password: "13112002",
-        },
-      });
+      const response = await axios(config);
 
       expect(response.status).to.equal(200);
     } catch (error) {
